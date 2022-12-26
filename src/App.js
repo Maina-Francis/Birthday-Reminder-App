6bb2d3943539425bfc91c1ce0017f42a274d962b
Francis Maina <sebastianfrancix@gmail.com>
2022-12-26 03:26:35 +0300

    added list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React, { useState } from "react";
+import { data } from "./data";
+import List from "./List";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main>
+      <section className="container">
+        <h3> 0 birthdays today</h3>
+        <List />
+        <button
+          type="button"
+          onClick={() => {
+            console.log("You clicked Clear all");
+          }}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
